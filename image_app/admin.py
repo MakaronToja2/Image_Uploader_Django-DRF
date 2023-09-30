caeb102c9e956cccb87079779342beffe839dd29
MakaronToja2 <wloch.jakub2@gmail.com>
2023-09-30 09:11:29 +0200

    Add models for AccountTier, UserProfile and Image

--- image_app/admin.py
+++ image_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import AccountTier, UserProfile, Image
 
-# Register your models here.
+admin.site.register(AccountTier)
+admin.site.register(UserProfile)
+admin.site.register(Image)
--- image_app/models.py
+++ image_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class AccountTier(models.Model):
+    name = models.CharField(max_length=50)
+    thumbnail_sizes = models.JSONField()
+    can_generate_expiring_liks = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    account_tier = models.ForeignKey(AccountTier, on_delete=models.SET_NULL, null=True, blank=True)
+
+class Image(models.Model):
+    user = models.ForeignKey(User, related_name='images', on_delete=models.CASCADE)
+    image_file = models.ImageField(upload_to='images/')
+    upload_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.name}'s Image {self.id}"
+
